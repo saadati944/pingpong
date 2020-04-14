5cc53c7c09b5e2ac6e3c4e762a67b128223386d7
saadati944 <saadati944@gmail.com>
2020-04-14 13:49:22 +0430

    Add menu function.

--- pingpong/Program.cs
+++ pingpong/Program.cs
@@ -12,5 +12,61 @@ namespace pingpong
         {
 
         }
+        static string menu(string[] items, string title)
+        {
+            Console.Clear();
+            Console.WriteLine(title);
+            int enterCount = entersCount(title) + 1;
+            int selected = 0, last = 0;
+            for (int i = 0; i < items.Length; i++)
+                Console.WriteLine((i == selected ? "  -->  " : "") + items[i] + (i == selected ? "  <--" : ""));
+            while (true)
+            {
+                ConsoleKeyInfo ck = Console.ReadKey(true);
+                if (ck.Key == ConsoleKey.DownArrow)
+                {
+                    selected++;
+                    if (selected > items.Length - 1)
+                        selected = 0;
+                }
+                else if (ck.Key == ConsoleKey.UpArrow)
+                {
+                    selected--;
+                    if (selected < 0)
+                        selected = items.Length - 1;
+                }
+                else if (ck.Key == ConsoleKey.Enter)
+                {
+                    if (selected == -1)
+                        return "";
+                    else return items[selected];
+                }
+                if (selected != last)
+                {
+                    Console.SetCursorPosition(0, enterCount + last);
+                    Console.Write(emptyString(12 + items[last].Length));
+                    Console.SetCursorPosition(0, enterCount + last);
+                    Console.Write(items[last]);
+                    Console.SetCursorPosition(0, enterCount + selected);
+                    Console.Write("  -->  " + items[selected] + "  <--");
+                }
+                last = selected;
+            }
+        }
+        static int entersCount(string v)
+        {
+            int i = 0;
+            foreach (char x in v)
+                if (x == '\n')
+                    i++;
+            return i;
+        }
+        static string emptyString(int len)
+        {
+            string s = "";
+            for (int i = 0; i < len; i++)
+                s += " ";
+            return s;
+        }
     }
 }
